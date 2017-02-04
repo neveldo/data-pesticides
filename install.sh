48975c0f637115577dfe8afca64d0c6074e8c631
neveldo <neveldo@gmail.com>
2017-02-04 17:32:22 +0100

    Add 2013 and 2014 datasets

--- install.sh
+++ install.sh
@@ -12,12 +12,14 @@ bin/console data-pesticides:load-dataset --type=pesticides --file=datasets/sourc
 bin/console data-pesticides:load-dataset --type=roles --file=datasets/source/fonctions.csv
 bin/console data-pesticides:load-dataset --type=departments --file=datasets/source/depts2016.csv
 
-bin/console data-pesticides:load-dataset --type=station_statements --file=datasets/source/analyses2007.csv --year=2007
-bin/console data-pesticides:load-dataset --type=station_statements --file=datasets/source/analyses2008.csv --year=2008
-bin/console data-pesticides:load-dataset --type=station_statements --file=datasets/source/analyses2009.csv --year=2009
-bin/console data-pesticides:load-dataset --type=station_statements --file=datasets/source/analyses2010.csv --year=2010
-bin/console data-pesticides:load-dataset --type=station_statements --file=datasets/source/analyses2011.csv --year=2011
-bin/console data-pesticides:load-dataset --type=station_statements --file=datasets/source/analyses2012.csv --year=2012
+bin/console data-pesticides:load-dataset --type=station_statements --file=datasets/source/ma_qp_fm_rcsrco_pesteso_2007.csv --year=2007
+bin/console data-pesticides:load-dataset --type=station_statements --file=datasets/source/ma_qp_fm_rcsrco_pesteso_2008.csv --year=2008
+bin/console data-pesticides:load-dataset --type=station_statements --file=datasets/source/ma_qp_fm_rcsrco_pesteso_2009.csv --year=2009
+bin/console data-pesticides:load-dataset --type=station_statements --file=datasets/source/ma_qp_fm_rcsrco_pesteso_2010.csv --year=2010
+bin/console data-pesticides:load-dataset --type=station_statements --file=datasets/source/ma_qp_fm_rcsrco_pesteso_2011.csv --year=2011
+bin/console data-pesticides:load-dataset --type=station_statements --file=datasets/source/ma_qp_fm_rcsrco_pesteso_2012.csv --year=2012
+bin/console data-pesticides:load-dataset --type=station_statements --file=datasets/source/ma_qp_fm_rcsrco_pesteso_2013.csv --year=2013
+bin/console data-pesticides:load-dataset --type=station_statements --file=datasets/source/ma_qp_fm_rcsrco_pesteso_2014.csv --year=2014
 
 bin/console data-pesticides:load-dataset --type=station_statements_total --file=datasets/source/moy_tot_quantif_2007.csv --year=2007
 bin/console data-pesticides:load-dataset --type=station_statements_total --file=datasets/source/moy_tot_quantif_2008.csv --year=2008
@@ -25,6 +27,8 @@ bin/console data-pesticides:load-dataset --type=station_statements_total --file=
 bin/console data-pesticides:load-dataset --type=station_statements_total --file=datasets/source/moy_tot_quantif_2010.csv --year=2010
 bin/console data-pesticides:load-dataset --type=station_statements_total --file=datasets/source/moy_tot_quantif_2011.csv --year=2011
 bin/console data-pesticides:load-dataset --type=station_statements_total --file=datasets/source/moy_tot_quantif_2012.csv --year=2012
+bin/console data-pesticides:load-dataset --type=station_statements_total --file=datasets/source/moy_tot_quantif_2013.csv --year=2013
+bin/console data-pesticides:load-dataset --type=station_statements_total --file=datasets/source/moy_tot_quantif_2014.csv --year=2014
 
 # Warmup API cache
 
